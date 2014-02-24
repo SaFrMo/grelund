fd4017f03149703418be5bb36873b43f284fa9f6
Sander <sandermoolin@gmail.com>
2014-02-23 21:56:29 -0500

    Fixed breathing issue on pawns

---
+++ Assets/scripts/MatchingGUISize.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchingGUISize : MonoBehaviour {
+
+	/*
+	 * Attach to a cube that you want a GUIBox to lay directly over.
+	 * Modify children in implementation as you see fit.
+	 */
+
+	protected GUIContent content = new GUIContent ("test");
+
+	protected void OnGUI () {
+
+		GUI.Box (new Rect (Camera.main.WorldToScreenPoint (renderer.bounds.min).x,
+	                       Camera.main.WorldToScreenPoint (-transform.position - renderer.bounds.extents).y,
+		                   Camera.main.WorldToScreenPoint (renderer.bounds.max).x - Camera.main.WorldToScreenPoint(renderer.bounds.min).x + 1f,
+		                   Camera.main.WorldToScreenPoint (renderer.bounds.max).y - Camera.main.WorldToScreenPoint(renderer.bounds.min).y + 1f), content);
+
+	}
+}
---
+++ Assets/scripts/PawnBehavior.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PawnBehavior : MonoBehaviour {
+
+	float minSize = .5f;
+	float maxSize = 2f;
+
+	float currentScale;
+
+	float rate = 1f;
+
+	bool expanding = true;
+
+	void Breathe() {
+		if (currentScale < maxSize && expanding) {
+			currentScale += rate * Time.deltaTime;
+		}
+		else if ((currentScale >= maxSize && expanding) || (currentScale <= minSize && !expanding)) {
+			expanding = !expanding;
+		}
+		else if (currentScale > minSize && !expanding) {
+			currentScale -= rate * Time.deltaTime;
+		}
+	}
+
+	void Start() {
+		transform.localScale *= minSize;
+		currentScale = minSize;
+	}
+
+	void Update () {
+		Breathe();
+		transform.localScale = Vector3.one * currentScale;
+	}
+}
