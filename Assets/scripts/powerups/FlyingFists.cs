178e8a097c0f97aabc561f607726b336b996b7ce
Sander <sandermoolin@gmail.com>
2014-03-05 16:25:54 -0500

    Powerup dev

---
+++ Assets/scripts/powerups/FlyingFists.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingFists : Powerup {
+
+	public GameObject flyingFistPrefab;
+
+	//new float LifeSpan = 5f;
+
+	void Start () {
+		LifeSpan = 5f;
+	}
+
+	protected override void Action() {
+		print ("ACTION!");
+	}
+
+}
---
+++ Assets/scripts/powerups/Powerup.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+	// BASE CLASS FOR ALL POWERUPS
+	// =============================
+	// All powerups:
+	//		X Activate on player touch
+	//		Have a lifespan, then are destroyed
+	//		Are destroyed when the go offscreen
+
+	protected Timer lifeTimer = null;
+	public virtual float LifeSpan { get; protected set; }
+	protected virtual void Action () {}
+
+
+	protected void OnCollisionEnter (Collision c) {
+		if (c.gameObject.CompareTag ("Player")) {
+
+			//==================================
+			// PUT THE ACTIVATION ANIMATION HERE
+			//==================================
+
+			if (lifeTimer == null) {
+				// sets the timer and makes the powerup invisible
+				GetComponent<MeshRenderer>().enabled = false;
+				GetComponent<BoxCollider>().enabled = false;
+				lifeTimer = new Timer (LifeSpan);
+			}
+			// keeps player from drifting
+			c.rigidbody.velocity = Vector3.zero;
+		}
+	}
+
+	protected void Update () {
+		if (lifeTimer != null) {
+			// runs Action() while the powerup is still "alive"
+			Action ();
+			// kills the powerup's effect after a set time
+			if (lifeTimer.RunTimer()) {
+				Destroy (gameObject);
+			}
+		}
+	}
+}
