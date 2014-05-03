b8472039ee957eb4d105acfa9cec1935f08feb4f
Rockslim <slotterpop@gmail.com>
2014-05-02 22:35:44 -0400

    Added score, enabled menus

---
+++ Assets/scripts/Menus/Score.js
@@ -0,0 +1,34 @@
+﻿var score1 : GUIText;
+
+var myScore : int = 0;
+var maxScore : int = 100000000000;
+
+function Update () {
+
+
+score1.text = "Score: " + myScore;
+
+
+if(myScore < maxScore) {
+
+myScore += 10;
+
+
+}
+
+
+
+if(Input.GetKeyDown("b")) {
+
+
+maxScore += 100;
+
+
+
+}
+
+
+
+
+
+}
